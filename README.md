13849dcae46a6076fb9360896c0dd315bcd4a5f1
dimsunv <dimsunv@gmail.com>
2022-11-14 00:42:05 +0300

    [08-ansible-06-modules]

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ansible Collection - boliwar.homework
 
 Documentation for the collection.
+
+##Installation
+
+```
+ansible-galaxy collection install git+https://github.com/dimsunv/my_collections.git,create_file
+```
---
+++ plugins/modules/file.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+# from turtle import width
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: ansible_file
+short_description: Module for creating files
+# If this is part of a collection, you need to use semantic versioning,
+# i.e. the version is of the form "2.5.0" and not "2.4".
+version_added: "1.0.0"
+description: This module creates files with content
+options:
+    path:
+        description: Path to the file
+        required: true
+        type: str
+    content:
+        description:
+            - Content of the file
+        required: true
+        type: str
+author:
+    - boliwar
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+- name: Test the file
+  netology.create_file:
+    path: "/tmp/test.txt"
+    content: "I can do it"
+'''
+
+RETURN = r'''
+'''
+
+import os
+from ansible.module_utils.basic import AnsibleModule
+
+def write_file(path, content):
+    with open(path, 'w') as f:
+        f.write(content)
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        path=dict(type='str', required=True),
+        content=dict(type='str', required=True)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    try:
+        file = open(module.params['path'])
+    except IOError as e:
+        write_file(module.params['path'], module.params['content'])
+        result['changed'] = True
+    else:
+        with file:
+            with open(module.params['path'], 'r') as f:
+                content = f.read()
+            if content != module.params['content']:
+                write_file(module.params['path'], module.params['content'])
+                result['changed'] = True
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
